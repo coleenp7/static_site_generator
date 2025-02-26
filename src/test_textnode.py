64df284ae744f082bd93833f87f4e7b36fa1b931
coleenp7 <coleenp7@gmail.com>
2025-02-26 13:03:20 -0700

    extraction added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,7 +8,136 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_text(self):
+        node = TextNode("This is a plain text node", TextType.NORMAL)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a plain text node")
 
+    def test_links(self):
+        node = TextNode("This is a link test node", TextType.LINKS, "boot.dev")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link test node")
+
+    def test_images(self):
+        node = TextNode("Alt text", TextType.IMAGES, "www.boot.dev/image.png")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "www.boot.dev/image.png")
+        self.assertEqual(html_node.props["alt"], "Alt text")
+
+    def test_no_delimiter(self):
+        node = TextNode("Hello world", TextType.NORMAL)
+        nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        assert len(nodes) == 1
+        assert nodes[0].text == "Hello world"
+        assert nodes[0].text_type == TextType.NORMAL
+
+    def test_basic_split(self):
+        nodes = [TextNode("hello `world` today", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        assert len(result) == 3
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == "world"
+        assert result[1].text_type == TextType.CODE
+        assert result[2].text == " today"
+        assert result[2].text_type == TextType.NORMAL
+    
+    def test_multiple_delimiter(self):
+        nodes = [TextNode("hello `world` and `python`", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        assert len(result) == 5
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == "world"
+        assert result[1].text_type == TextType.CODE
+        assert result[2].text == " and "
+        assert result[2].text_type == TextType.NORMAL
+        assert result[3].text == "python"
+        assert result[3].text_type == TextType.CODE
+        assert result[4].text == ""
+        assert result[4].text_type == TextType.NORMAL
+
+    def test_empty_content(self):
+        nodes = [TextNode("hello `` world", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        assert len(result) == 3
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == ""
+        assert result[1].text_type == TextType.CODE
+        assert result[2].text == " world"
+        assert result[2].text_type == TextType.NORMAL
+
+    def test_double_delimiter(self):
+        nodes = [TextNode("hello **cruel** world", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        assert len(result) == 3
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == "cruel"
+        assert result[1].text_type == TextType.BOLD
+        assert result[2].text == " world"
+        assert result[2].text_type == TextType.NORMAL
+
+    def test_italics_delimiter(self):
+        nodes = [TextNode("hello _fun_ world", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+        assert len(result) == 3
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == "fun"
+        assert result[1].text_type == TextType.ITALIC
+        assert result[2].text == " world"
+        assert result[2].text_type == TextType.NORMAL
+
+    def test_multiple_nodes(self):
+        nodes = [TextNode("hello _fun_ world", TextType.NORMAL), TextNode("Joy _to_ the world", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+        assert len(result) == 6
+        assert result[0].text == "hello "
+        assert result[0].text_type == TextType.NORMAL
+        assert result[1].text == "fun"
+        assert result[1].text_type == TextType.ITALIC
+        assert result[2].text == " world"
+        assert result[2].text_type == TextType.NORMAL
+        assert result[3].text == "Joy "
+        assert result[3].text_type == TextType.NORMAL
+        assert result[4].text == "to"
+        assert result[4].text_type == TextType.ITALIC
+        assert result[5].text == " the world"
+        assert result[5].text_type == TextType.NORMAL
+
+    def test_no_second_delimiter(self):
+        nodes = [TextNode("hello _fun world", TextType.NORMAL)]
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    def test_incorrect_delimiter(self):
+        nodes = [TextNode("hello_ fun world_", TextType.NORMAL)]
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def simple_markdown(self):
+        text = "![test](example.com)"
+        result = extract_markdown_images(text)
+        print(result)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+        "This is text with a [link to somewhere](https://example.com)"
+        )
+        self.assertListEqual([("link to somewhere", "https://example.com")], matches)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import re
+from htmlnode import *
 
 class TextType(Enum):
 
@@ -22,3 +24,57 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    def text_node_to_html_node(self):
+        if not isinstance(self.text_type, TextType):
+            raise Exception("Invalid text type in textnode")
+        elif self.text_type == TextType.LINKS:
+            return LeafNode("a", self.text, {"href": self.url})
+        elif self.text_type == TextType.IMAGES:
+            return LeafNode("img", "", {"src":self.url, "alt": self.text})
+        elif self.text_type == TextType.NORMAL:
+            return LeafNode(None, self.text)
+        elif self.text_type == TextType.BOLD:
+            return LeafNode("b", self.text)
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode("i", self.text)
+        elif self.text_type == TextType.CODE:
+            return LeafNode("code", self.text)
+        else:
+            raise Exception("error in conversion")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    returnable = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            returnable.append(node)
+        else:
+            parts = []
+            text = node.text
+            while True:
+                d_start = text.find(delimiter)
+                if d_start == -1:
+                    parts.append(TextNode(text, TextType.NORMAL))
+                    break
+                if d_start > 0 and not text[d_start - 1].isspace():
+                    raise Exception("Invalid delimiter attached to word")
+                before_start = text[0:d_start]
+                if before_start.count(delimiter) > 0:
+                    raise Exception("Invalid delimiter order")
+
+                d_end = text.find(delimiter, d_start + len(delimiter))
+                if d_end == -1:
+                    raise Exception("Second delimiter not found")
+                
+                parts.append(TextNode(text[0:d_start], TextType.NORMAL))
+                parts.append(TextNode(text[d_start + len(delimiter):d_end], text_type))
+                text = text[d_end + len(delimiter):]
+            returnable.extend(parts)
+                        
+    return returnable
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return (re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text))
